af12dfb5d988b7c616ebb9b96f94b95fd95cf796
Daniel.C <cheadaniel.95@gmail.com>
2023-09-14 10:56:20 +0200

    page de contact a la discord ok + schema  base donnée

--- api/public/js/contact.js
+++ api/public/js/contact.js
@@ -1,89 +1,83 @@
+const messageContainer = document.getElementById('messageContainer');
+const messageForm = document.getElementById('messageForm');
+const currentUrl = window.location.href;
 // Fonction pour effectuer une requête AJAX et insérer le résultat dans le conteneur
 function fetchAndInsertData(url, container) {
-    if (container.innerHTML == '') {
-        fetch(url)
-            .then(response => response.text())
-            .then(data => {
-                container.innerHTML = data;
-            })
-            .catch(error => {
-                console.error('Erreur réseau', error);
-            });
-    } else {
-        const newDiv = document.createElement('div');
-        console.log(url)
-        fetch(url)
-            .then(response => response.text())
-            .then(data => {
-                newDiv.innerHTML = data;
-                container.appendChild(newDiv);
-            })
-            .catch(error => {
-                console.error('Erreur réseau', error);
-            });
-    }
-
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            container.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Erreur réseau', error);
+        });
 }
 
+function sendMessage(url, container) {
+    const messageContainer = document.getElementById('messageContainer');
 
+    const messageFormContent = document.getElementById('messageContent');
+    const content = messageFormContent.value;
+    //messageContainer.innerHTML = ''
+
+    fetch(url + '/message/send', {
+        method: 'POST',
+        body: JSON.stringify({ content }),
+        headers: {
+            'Content-Type': 'application/json', // Définissez le type de contenu JSON
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Insérez le résultat dans le conteneur
+
+            // Réinitialisez le formulaire ou effectuez d'autres actions en fonction de la réponse
+            if (data.success) {
+                // Réinitialisez le formulaire
+                messageForm.reset();
+                messageContainer.innerHTML = '\n'
+                fetchAndInsertData(url, container);
+            }
+        })
+        .catch(error => {
+            console.error('Erreur réseau', error);
+        });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-    const messageContainer = document.getElementById('messageContainer');
+
+    //console.log(messageContainer, messageContainer.innerHTML)
 
     // Écoutez les clics sur les liens avec la classe "sendMessageLink"
     document.querySelectorAll('.sendMessageLink').forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
             const userId = this.getAttribute('data-id');
-            const currentUrl = window.location.href;
             const urlWithUserId = currentUrl + '/' + userId;
+            messageContainer.innerHTML = '\n'
 
             // Appel de la fonction pour effectuer la première requête AJAX
             fetchAndInsertData(urlWithUserId, messageContainer)
 
             // Appel de la fonction pour effectuer la deuxième requête AJAX
-            setTimeout(fetchAndInsertData(urlWithUserId + '/message', messageContainer),2000)
-            
 
+            messageForm.setAttribute('data-id', userId);
+            messageForm.style.display = 'block';
+
+            //sendMessage(urlWithUserId, messageContainer)
             //3eme requete 
-            setTimeout(function () {
-                const messageForm = document.forms['message_form'];
-                //const messageContainer = document.getElementById('messageContainer');
-
-                messageForm.addEventListener('submit', function (event) {
-                    event.preventDefault();
-                    const messageFormContent = document.getElementById('message_form_content');
-                    const content = messageFormContent.value;
-
-                    fetch(urlWithUserId + '/message/send', {
-                        method: 'POST',
-                        body: JSON.stringify({ content }),
-                        headers: {
-                            'Content-Type': 'application/json', // Définissez le type de contenu JSON
-                        },
-                    })
-                        .then(response => response.json())
-                        .then(data => {
-                            // Insérez le résultat dans le conteneur
-                            
-                            // Réinitialisez le formulaire ou effectuez d'autres actions en fonction de la réponse
-                            if (data.success) {
-                                // Réinitialisez le formulaire
-                                console.log(data,messageForm,messageContainer)
-                                messageForm.innerHTML = '';
-                                messageContainer.innerHTML = ''
-                                fetchAndInsertData(urlWithUserId, messageContainer);
-                                setTimeout(fetchAndInsertData(urlWithUserId + '/message', messageContainer),10000)
-                                console.log(messageForm,messageContainer)
-
-                            }
-                        })
-                        .catch(error => {
-                            console.error('Erreur réseau', error);
-                        });
-                })
-            }, 2000)
-
         })
+
+
     })
 })
+
+messageForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const userId = this.getAttribute('data-id');
+    const urlWithUserId = currentUrl + '/' + userId;
+    sendMessage(urlWithUserId, messageContainer)
+})
+
+
+
