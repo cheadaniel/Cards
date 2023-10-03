5a95a7bb81d5200b0e4046cbfeefda560e225f82
Daniel.C <cheadaniel.95@gmail.com>
2023-10-03 12:17:15 +0200

    debut de la page des jeux + form pour en creer un et lien vers la page spécifique d'un jeu

---
+++ api/src/Controller/GameController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use App\Form\GameFormType;
+use App\Repository\GameRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    #[Route('/games', name: 'games')]
+    public function index(GameRepository $gameRepository): Response
+    {
+        $games = $gameRepository->findAll();
+
+        return $this->render('game/index.html.twig', [
+            'games' => $games,
+        ]);
+    }
+
+    #[Route('/games/{gameName}', name: 'game')]
+    public function game($gameName): Response
+    {
+
+        return $this->render('game/game.html.twig', [
+            'game' => $gameName,
+        ]);
+    }
+
+    #[Route('admin/games/create', name:'create_game')]
+    public function create_game(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $game = new Game();
+
+        $form = $this->createForm(GameFormType::class, $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($game);
+            $entityManager->flush();
+            return $this->redirectToRoute('games');
+        } 
+
+
+        return $this->render('game/createGame.html.twig', [
+            'gameForm' => $form->createView(),
+        ]);
+    }
+}
---
+++ api/src/Form/GameFormType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Game;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('Name', TextareaType::class, [
+            'label' => 'Nom'
+        ])
+        ->add('Image', TextareaType::class, [
+            'label' => 'Url de l\'image'
+        ])
+        ->add("Envoyer", SubmitType::class, [
+            'label' => 'Envoyer',
+        ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Game::class,
+        ]);
+    }
+}
