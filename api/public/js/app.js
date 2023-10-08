570cf933453c5441db93c90a02b36b778a04b573
Daniel.C <cheadaniel.95@gmail.com>
2023-10-08 05:39:21 +0200

    header css

--- api/public/js/app.js
+++ api/public/js/app.js
@@ -1,6 +1,19 @@
 const toggleButton = document.querySelector('#mode-toggle-button');
+const divToggleButton = document.querySelector('#mode');
 const body = document.body;
 
 toggleButton.addEventListener('click', () => {
+    const currentText = toggleButton.textContent;
+
+    if (currentText === 'Mode Light') {
+        toggleButton.textContent = 'Dark Mode';
+    } else {
+        toggleButton.textContent = 'Light Mode';
+    }
+    body.classList.toggle('light-mode');
     body.classList.toggle('dark-mode');
+    divToggleButton .classList.toggle('light-mode');
+    divToggleButton.classList.toggle('dark-mode');
+    toggleButton.classList.toggle('light-mode');
+    toggleButton.classList.toggle('dark-mode');
 });
